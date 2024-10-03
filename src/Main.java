375490cd78c14bf180dbd18cac06f23e751ba884
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-10-03 15:59:12 +0200

    Stuff V1.3.1

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,10 @@ public class Main {
         Personnage Hero = new Personnage("Nao","Magicien");
         Personnage Hero2 = new Personnage("Lucas","Guerrier");
         System.out.println(Hero);
-        System.out.println(Hero2);
         EquipementOffensif EquipementOffensif = new EquipementOffensif("Baton", "Sort");
         System.out.println(EquipementOffensif);
-        EquipementDefensif EquipementDefensif = new EquipementDefensif("","Philtre");
+        System.out.println(Hero2);
+        EquipementDefensif EquipementDefensif = new EquipementDefensif("Sac","Philtre");
         System.out.println(EquipementDefensif);
-
     }
 }
