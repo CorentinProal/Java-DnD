a212a70db566e303bb89c7d16865c655dd595249
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-10-03 10:07:57 +0200

    init et dÃéménagement

--- src/Demenagement.java
+++ src/Demenagement.java
@@ -1,20 +1,12 @@
 public class Demenagement {
-    public static void main(String[] args) {
-        // Variables
-        int totalDesCartons = 34;
-        int placeDansCamion = 9;
-        int nombreDeVoyages;
+    public static void main (String[] args) {
+        int totalCartons = 34;
+        int capaciteCamion = 9;
 
-        nombreDeVoyages = (int) Math.ceil((double) totalDesCartons / placeDansCamion);
-
-        System.out.println("Cartons à déménager : " + totalDesCartons);
-        System.out.println("Place dans le camion : " + placeDansCamion);
-        System.out.println("Nombre de voyages nécessaires : " + nombreDeVoyages);
-
-        for (int voyage = 1; voyage <= nombreDeVoyages; voyage++) {
-            int cartonsRestants = totalDesCartons - (voyage - 1) * placeDansCamion;
-            int cartonsDansCamion = Math.min(cartonsRestants, placeDansCamion);
-            System.out.println("Voyage " + voyage + " : " + cartonsDansCamion + " cartons dans le camion.");
+        int nombreDeVoyages = totalCartons / capaciteCamion;
+        if (totalCartons % capaciteCamion > 0) {
+            nombreDeVoyages++;
         }
-    }
+        System.out.println("Nombre de Voyages nécessaires : " + nombreDeVoyages);
+            }
 }
