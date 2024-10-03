5d4e9626d943ef74d6e9805f08b2991a6917dcb0
Corentin Proal <corentin.proal@Digital-Grenoble.local>
2024-10-03 14:02:02 +0200

    Créa Perso V1.2 MàJ

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type de personnage (Guerrier/Magicien) : ");
+        String type = scanner.nextLine();
+        System.out.print("Nom du personnage : ");
+        String name = scanner.nextLine();
+        Personnage personnage = new Personnage(name, type);
+        personnage.afficher();
+        scanner.close();
 
+//        Personnage personnage = new Personnage("Remi", "Guerrier");
+//        Personnage personnage1 = new Personnage("Nao","Magicien");
+//        personnage.afficher();
+//        personnage1.afficher();
     }
 }
-
-
-
-
-//Scanner sc = new Scanner(System.in);
-//        System.out.println("Entre ton pseudo & ta classe : ");
-//        String classe = sc.nextLine();
-//        System.out.println("Ta classe : " + classe);
-//        String name = sc.nextLine();
-//        System.out.println("Ton pseudo : " + name);
--- src/Personnage.java
+++ src/Personnage.java
@@ -1,28 +1,39 @@
-import java.util.Scanner;
-
 public class Personnage {
-    String name;
-    String type;
+    private String name;
+    private String type;
+
+    public Personnage() {
+        this.name = "";
+        this.type = "";
+    }
 
     public Personnage(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
-    public void afficher() {
-        System.out.println("Nom: " + name + ", Type: " + type);
+    public String getName() {
+        return name;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Type de personnage (Guerrier/Magicien) : ");
-        String type = scanner.nextLine();
-        System.out.print("Nom du personnage : ");
-        String name = scanner.nextLine();
-        Personnage personnage = new Personnage(name, type);
-        personnage.afficher();
-        scanner.close();
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        return "Nom: " + name + ", Type: " + type;
+    }
+
+    public void afficher() {
+        System.out.println(this.toString());
     }
 }
-
 
